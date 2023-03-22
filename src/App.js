5e154b96566f3f8b8cff59e6cc228147901913b2
JeradeaSimmons <JeradeaSimmons@gmail.com>
2023-03-22 16:22:33 -0600

    Still playing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div>
         <p>
           RAW THOUGHTS PODCAST
         </p>
+        </div>
+        <div class="">
+          <a class="selectable" title="Facebook Link" href="https://www.facebook.com/makenzie.tiegs208"><i
+              class="mdi mdi-facebook text-white"></i></a>
+          <a class="selectable" href="https://www.instagram.com/rawthoughts.podcast/"><i
+              class="mdi mdi-instagram text-white"></i></a>
+          <a class="selectable" href="https://linktr.ee/rawthoughts.podcast"><i
+              class="mdi mdi-link text-white"></i></a>
+          <a class="selectable" href="https://www.youtube.com/channel/UCIQkzGm10CBfiqaOokJWygg"><i
+              class="mdi mdi-youtube text-white"></i></a>
+          <a class="selectable" href="https://open.spotify.com/show/5Q324MLEVXPUMk6JnaNgXa"><i
+              class="mdi mdi-spotify text-white"></i></a>
+          </div>
+        
       
       </header>
+     <body>
+      Main stuff
+     </body>
+     <footer>
+      Brought to you by Raw Thoughts Podcast 2021
+     </footer>
     </div>
   );
 }
