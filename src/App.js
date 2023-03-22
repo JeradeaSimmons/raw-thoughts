f10587307d5615ac8a6fece72014c726231c2f27
JeradeaSimmons <JeradeaSimmons@gmail.com>
2023-03-22 16:56:11 -0600

    piecing together

--- src/App.js
+++ src/App.js
@@ -25,9 +25,47 @@ function App() {
         
       
       </header>
-     <body>
-      Main stuff
+      
+     <body className="App-body">
+       
+       <div class="row d-flex justify-content-evenly p-2">
+        
+        <div class="col-md-3">
+        <h5>
+            Welcome to Raw Thoughts with MaKenzie Tiegs where I dive deep into conversations with people in the Treasure
+            Valley to understand their life experiences. From a Paramedic to now a Real Estate Agent, connecting with
+            people
+            is my passion, Raw Thoughts is a place to feel safe, heard and welcomed. We all deserve to feel good in this
+            life and I am here to share that goodness with others.
+          </h5>
+          <a
+              href="https://docs.google.com/forms/d/1vhV4vzKYp8QXd_Tyy8slo5opL3r2lkLHYVYGRMQ3-m4/viewform?edit_requested=true"><button
+                class="rounded text-black mt-3">
+                <h3>Want to be a Guest?</h3>
+              </button></a>
+        </div>
+        <div class="col-md-3">
+          <img src="Kenzie11.jpg" alt="Kenzie" class="img-fluid"></img>
+        </div>
+        <div class="col-md-3">
+          
+        </div>
+        
+        
+        
+        
+       </div>
+       
+         
+        
+         
+    
+         
+      
+
+     
      </body>
+     
      <footer>
       Brought to you by Raw Thoughts Podcast 2021
      </footer>
